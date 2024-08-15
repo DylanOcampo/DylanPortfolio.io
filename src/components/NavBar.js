a12a5437ca8f144236bc094f42d9a23e7ab42188
Dylan Ocampo <dylanomar@live.com.mx>
2024-08-15 10:23:09 -0600

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -61,7 +61,7 @@ export const NavBar = () => {
                         type="button" 
                         onClick={(e) => {
                           e.preventDefault();
-                          window.location.href='https://drive.google.com/file/d/1FsNVaMzP1vB6j84PxqTmjQPTt89gJ2hf/view?usp=drive_link';
+                          window.location.href='https://drive.google.com/file/d/1mGZBPx25Enys2brJM1kvT1si_fG24Zxz/view?usp=sharing';
                           }}
                     > Check CV</button>
             </span>
